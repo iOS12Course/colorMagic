dbf03b318de1d3f4b30fafe5e99bcc4d6df6c959
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-04-16 21:31:44 -0500

    Initial Commit

---
+++ ColorMagic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorMagic
+//
+//  Created by Ricardo Herrera Petit on 4/16/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
